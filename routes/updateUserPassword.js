91858980675af313568d638db7376c5520dc4945
Aman Kumar Singh <amansingh99170@gmail.com>
2024-11-01 16:59:13 +0530

    update user password change

--- routes/updateUserPassword.js
+++ routes/updateUserPassword.js
@@ -2,10 +2,11 @@ import express from 'express';
 const router = express.Router();
 import {User} from "../db/models/Users.js";
 import bcrypt from 'bcryptjs';
+import { fetchUser } from '../middleware/fetchUser.js';
 
-router.patch('/:id', async (req, res) => {
+router.patch('/:id', fetchUser,async (req, res) => {
     const { oldPassword, newPassword } = req.body
-    const userID = req.params.id
+    const userID = req.id
     try {
         const user = await User.findById(userID)
         if (!user) {
@@ -16,9 +17,16 @@ router.patch('/:id', async (req, res) => {
             return res.status(400).json({ message: 'Invalid old password' })
         }
         const hashedPassword = await bcrypt.hash(newPassword, 10)
-        user.password = hashedPassword
-        user.save()
-        res.status(200).json({message:"Password updated successfully"})
+        const updatedUser = {}
+        updatedUser.password = hashedPassword
+        const finalUpdatedUser = await User.findByIdAndUpdate(userID,{$set:updatedUser})
+        if(finalUpdatedUser){
+            res.status(200).json({message:"Password updated successfully"})
+        }
+        else{
+        res.status(400).json({message:"Bad Request"})
+        }
+        
     }
     catch (error) {
         console.error(error);
